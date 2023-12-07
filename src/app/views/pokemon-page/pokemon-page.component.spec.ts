7587515149c433e657ba0a4d38f6e678ad7fdb81
Nanna <nanaa.gonzalez@gmail.com>
2023-12-07 17:19:40 +0100

    Add pokemonPage component

---
+++ src/app/views/pokemon-page/pokemon-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemonPageComponent } from './pokemon-page.component';
+
+describe('PokemonPageComponent', () => {
+  let component: PokemonPageComponent;
+  let fixture: ComponentFixture<PokemonPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PokemonPageComponent]
+    });
+    fixture = TestBed.createComponent(PokemonPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
