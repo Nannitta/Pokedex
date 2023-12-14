7d149e3b4e99f170e9cec4f7a455102edb057a5d
Nanna <nanaa.gonzalez@gmail.com>
2023-12-14 19:02:01 +0100

    Add cache-control

--- src/services/poke-api.service.ts
+++ src/services/poke-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AbilityInfo, PokemonInfo, PokemonList, PokemonMoreInfo } from 'src/models/types/types';
 
@@ -10,16 +10,22 @@ export class PokeApiService {
 
   constructor(private http: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Cache-Control': 'public, max-age=31536000'
+    })
+  }
+
   getPokemonList(): Observable<any> {
-    return this.http.get<PokemonList>('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+    return this.http.get<PokemonList>('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151', this.httpOptions)
   }
 
   getPokemonInfo(number: number): Observable<PokemonInfo> {
-    return this.http.get<PokemonInfo>(`https://pokeapi.co/api/v2/pokemon/${number}`)
+    return this.http.get<PokemonInfo>(`https://pokeapi.co/api/v2/pokemon/${number}`, this.httpOptions)
   }
 
   getPokemonDescription(number: number): Observable<PokemonMoreInfo> {
-    return this.http.get<PokemonMoreInfo>(`https://pokeapi.co/api/v2/pokemon-species/${number}`)
+    return this.http.get<PokemonMoreInfo>(`https://pokeapi.co/api/v2/pokemon-species/${number}`, this.httpOptions)
   }
 
   getAbilityDescription(url: string): Observable<AbilityInfo> {
